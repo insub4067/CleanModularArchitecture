0720fb24061e6acedcbfca738ecd98ca1b18395c
insub <vb15@kakao.com>
2023-12-16 13:17:27 +0900

    test 케이스 추가

--- CleanModularArchitectureTests/CleanModularArchitectureTests.swift
+++ CleanModularArchitectureTests/CleanModularArchitectureTests.swift
@@ -12,9 +12,49 @@ import XCTest
 @testable import DataSource
 @testable import Domain
 
+// Stub 을 주입하여 테스트하는 경우
+final class ContentViewModelTestsWithStub: XCTestCase {
+    
+    func test_네트워크실패한경우() {
+        
+        let repository = FailUserNetwork()
+        let usecase = GetUserUseCase(userRepository: repository)
+        let sut = ContentViewModel(getUserUseCase: usecase)
+        
+        // When
+        sut.getUser()
+        
+        // Then
+        let expectation = expectation(description: "1초 후 테스트 실행")
+        let result = XCTWaiter.wait(for: [expectation], timeout: 1.0)
+        guard result == XCTWaiter.Result.timedOut else {
+            return XCTFail("테스트 실패")
+        }
+        XCTAssertNotNil(sut.error)
+    }
+    
+    func test_네트워크성공한경우() {
+        
+        // Given
+        var repository = MockUserNetwork()
+        let usecase = GetUserUseCase(userRepository: repository)
+        let sut = ContentViewModel(getUserUseCase: usecase)
+        
+        // When
+        sut.getUser()
+        
+        // Then
+        let expectation = expectation(description: "1초 후 테스트 실행")
+        let result = XCTWaiter.wait(for: [expectation], timeout: 1.0)
+        guard result == XCTWaiter.Result.timedOut else {
+            return XCTFail("테스트 실패")
+        }
+        XCTAssertEqual(sut.fetchedUser?.login, "insub4067")
+    }
+}
 
-final class ContentViewModelTests: XCTestCase {
-
+// 구현체에 기대값을 할당하여 테스트하는 경우
+final class ContentViewModelTestsWithAssigment: XCTestCase {
     
     func test_네트워크실패한경우() {
         
@@ -67,5 +107,28 @@ final class ContentViewModelTests: XCTestCase {
             return XCTFail("테스트 실패")
         }
         XCTAssertEqual(sut.fetchedUser?.login, mock.login)
+    }
+}
+
+// 항상 실패는 반환하는 Fake UserRepository
+struct FailUserNetwork: UserRepositoriable {
+    var getUser: (String) -> AnyPublisher<Domain.UserEntity, Error> = { _ in
+        Fail(error: NSError(domain: "Fail", code: -1))
+            .eraseToAnyPublisher()
+    }
+}
+
+// 항상 Mock을 반환하는 Fake UserRepository
+struct MockUserNetwork: UserRepositoriable {
+    var getUser: (String) -> AnyPublisher<Domain.UserEntity, Error> = { _ in
+        let mock = UserEntity(
+            login: "insub4067",
+            avatarUrl: "https://avatars.githubusercontent.com/u/85481204?v=4",
+            name: "insub",
+            bio: "iOS Developer"
+        )
+        return Just(mock)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
     }
 }
